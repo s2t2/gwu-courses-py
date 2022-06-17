2101197cd456be8e53983d9c15b4f1bcfda47ee6
MJ Rossetti <s2t2@users.noreply.github.com>
2022-06-17 13:14:23 -0400

    Parse page for courses

--- app/parser.py
+++ app/parser.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 from pandas import DataFrame
 
 
+
 class PageParser:
     """Parses a previously downloaded HTML page"""
     def __init__(self, html_filepath):
@@ -14,10 +15,45 @@ class PageParser:
             contents = html_file.read()
             self.soup = BeautifulSoup(contents, features="html.parser")
 
-
     @cached_property
     def courses(self):
+        """
+        Looking for:
+
+            <table class="courseListing basicTable courseListingSetWidths" style="background-color:#006699;">
+
+        """
         records = []
 
-        breakpoint()
-        #tables = self.soup.find_all("article", "placard")
+        tables = self.soup.find_all("table", "courseListing")
+
+        for table in tables:
+            course_row = table.find("tr", "crseRow1")
+            comments_row = table.find("tr", "crseRow2")
+
+            cells = course_row.find_all("td")
+            #vals = [clean_text(cell.text) for cell in cells]
+            #> ['OPEN', '71511', 'EMSE1001', '10', 'Introduction to Systems Engineering', '1.00', '', 'TOMP 406', 'F09:35AM - 10:25AM', '08/29/22 - 12/12/22', 'Linked']
+
+            #subj, number = [span.text.strip() for span in cells[2].find_all("span")]
+            short_code = " ".join( [span.text.strip() for span in cells[2].find_all("span")])
+            comments = comments_row.text.strip().replace("\n","").replace("\t","").replace("\xa0\xa0"," ")
+
+            record = {
+                "availability": cells[0].text,
+                "crn": cells[1].text,
+                "short_code": short_code,
+                "section": cells[3].text,
+                "title": cells[4].text,
+                "credits": cells[5].text.strip(), # this can be like "0.00 OR   3.00"
+                "instructor": cells[6].text,
+                "location": cells[7].text,
+                "time_range": cells[8].text, # FYI: the first char is the day M/T/W/R/F, etc
+                "date_range": cells[9].text,
+                "comments": comments
+            }
+            records.append(record)
+
+        df = DataFrame(records)
+        print(df.head())
+        return df
---
+++ test/parser_test.py
@@ -0,0 +1,32 @@
+
+import os
+
+from pandas import DataFrame
+
+from app.parser import PageParser
+
+from conftest import MOCK_EXPORTS_DIRPATH
+
+
+def test_page_parser():
+
+    html_filepath = os.path.join(MOCK_EXPORTS_DIRPATH, "EMSE", "page_1.html")
+    parser = PageParser(html_filepath=html_filepath)
+
+    courses = parser.courses
+    assert isinstance(courses, DataFrame)
+    assert len(courses) == 20
+
+    assert courses.iloc[0].to_dict() == {
+        'availability': 'OPEN',
+        'crn': '71511',
+        'short_code': 'EMSE 1001',
+        'section': '10',
+        'title': 'Introduction to Systems Engineering',
+        'credits': '1.00',
+        'instructor': '',
+        'location': 'TOMP 406',
+        'time_range': 'F09:35AM - 10:25AM',
+        'date_range': '08/29/22 - 12/12/22',
+        'comments': 'Comments: Also register for laboratory section: EMSE 1001.30. This course meets: 10/14/2022 - 12/9/2022. Registration restricted to SEAS freshmen only. Find Books'
+    }
