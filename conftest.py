c1735e2dbd7bd0e0492c2981dcc6e54038ab4ec2
MJ Rossetti <s2t2@users.noreply.github.com>
2024-05-08 17:56:06 -0400

    Test page parsing

--- conftest.py
+++ conftest.py
@@ -2,5 +2,6 @@
 
 import os
 
+TEST_DIRPATH = os.path.join(os.path.dirname(__file__), "test")
 
-MOCK_EXPORTS_DIRPATH = os.path.join(os.path.dirname(__file__), "test", "mock_exports")
+MOCK_EXPORTS_DIRPATH = os.path.join(TEST_DIRPATH, "mock_exports")
