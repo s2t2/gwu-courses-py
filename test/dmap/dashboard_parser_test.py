3cec2ff164165bc69b72b794416a13146ef075f3
MJ Rossetti <s2t2@users.noreply.github.com>
2024-05-15 18:19:34 -0400

    Stop code sprawl

--- test/dmap/dashboard_parser_test.py
+++ test/dmap/dashboard_parser_test.py
@@ -1,15 +1,18 @@
 
-from pprint import pprint
+import os
+#from pprint import pprint
 
 import pytest
 
-from conftest import CI_ENV, DASHBOARD_1_FILEPATH, DASHBOARD_2_FILEPATH
+from conftest import CI_ENV, TEST_DIRPATH
 from app.html_helpers import read_and_parse_mhtml
 from app.dmap.dashboard_parser import DashboardParser
 
-
 SKIP_REASON = "Dashboard pages are ignored right now for privacy. Can remove skip once pages are more completely redacted."
 
+MOCK_DMAP_DIRPATH = os.path.join(TEST_DIRPATH, "mock_dmap")
+DASHBOARD_1_FILEPATH = os.path.join(MOCK_DMAP_DIRPATH, "dashboard-1-redacted.mhtml")
+DASHBOARD_2_FILEPATH = os.path.join(MOCK_DMAP_DIRPATH, "dashboard-2-redacted.mhtml")
 
 @pytest.fixture(scope="module")
 def dashboard_1_source():
@@ -19,6 +22,17 @@ def dashboard_1_source():
 def dashboard_2_source():
     return read_and_parse_mhtml(DASHBOARD_2_FILEPATH)
 
+
+
+@pytest.mark.skipif(CI_ENV, reason=SKIP_REASON)
+def test_student_info(dashboard_1_source, dashboard_2_source):
+    parser = DashboardParser(dashboard_1_source)
+    assert parser.student_id == "G11111111"
+    assert parser.student_name == "LAST_NAME_1, FIRST_NAME_1 MIDDLE_NAME_1"
+
+    parser = DashboardParser(dashboard_2_source)
+    assert parser.student_id == "G22222222"
+    assert parser.student_name == "LAST_NAME_2, FIRST_NAME_2 MIDDLE_INITIAL_2"
 
 
 @pytest.mark.skipif(CI_ENV, reason=SKIP_REASON)
